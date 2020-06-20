1f61b7f6af00443c9b72444337ca239d2c1c3142
yosuapurnomo <yosuapurnomo321@gmail.com>
2020-06-20 22:35:43 +0700

    next commit

---
+++ Kmean.py
@@ -0,0 +1,85 @@
+from builtins import print
+
+import numpy as np
+import pandas as pd
+
+
+class kmean:
+    def __init__(self, r, g, b):
+        self.r = r
+        self.g = g
+        self.b = b
+        self.df = pd.read_excel('warna.xlsx', header=0, index_col=0)
+        print(self.df)
+        # Average Merah
+        self.R_merah = self.df['R'][self.df['Warna'] == 'merah'].mean()
+        self.G_merah = self.df['G'][self.df['Warna'] == 'merah'].mean()
+        self.B_merah = self.df['B'][self.df['Warna'] == 'merah'].mean()
+        # Avarega Hijau
+        self.R_hijau = self.df['R'][self.df['Warna'] == 'hijau'].mean()
+        self.G_hijau = self.df['G'][self.df['Warna'] == 'hijau'].mean()
+        self.B_hijau = self.df['B'][self.df['Warna'] == 'hijau'].mean()
+        # Average Biru
+        self.R_biru = self.df['R'][self.df['Warna'] == 'biru'].mean()
+        self.G_biru = self.df['G'][self.df['Warna'] == 'biru'].mean()
+        self.B_biru = self.df['B'][self.df['Warna'] == 'biru'].mean()
+        # Average kuning
+        self.R_kuning = self.df['R'][self.df['Warna'] == 'kuning'].mean()
+        self.G_kuning = self.df['G'][self.df['Warna'] == 'kuning'].mean()
+        self.B_kuning = self.df['B'][self.df['Warna'] == 'kuning'].mean()
+        # Average Biru
+        self.R_putih = self.df['R'][self.df['Warna'] == 'putih'].mean()
+        self.G_putih = self.df['G'][self.df['Warna'] == 'putih'].mean()
+        self.B_putih = self.df['B'][self.df['Warna'] == 'putih'].mean()
+
+        self.Hasil = 0
+
+    def check(self):
+        warna = ""
+        for i in range(len(self.df)):
+            if [self.df.loc[i][0], self.df.loc[i][1], self.df.loc[i][2]] == [self.r, self.g, self.b]:
+                print("Data sudah ada diDataSet")
+                warna = self.df.loc[i][3]
+                break
+        else:
+            print("Data Belum ada diDataset")
+            warna = self.testing()
+            new_rows = {'R':self.r, 'G':self.g, 'B':self.b, 'Warna':warna}
+            self.df = self.df.append(new_rows, ignore_index=True)
+            self.df.to_excel('warna.xlsx')
+        return warna
+
+    def testing(self):
+        may_merah = (abs((self.r - self.R_merah)) + abs((self.g - self.G_merah)) + abs((self.b - self.B_merah)))
+        may_hijau = (abs(self.r - self.R_hijau) + abs((self.g - self.G_hijau)) + abs((self.b - self.B_hijau)))
+        may_biru = (abs(self.r - self.R_biru) + abs((self.g - self.G_biru)) + abs((self.b - self.B_biru)))
+        may_kuning = (abs(self.r - self.R_kuning) + abs((self.g - self.G_kuning)) + abs((self.b - self.B_kuning)))
+        may_putih = (abs(self.r - self.R_putih) + abs((self.g - self.G_putih)) + abs((self.b - self.B_putih)))
+
+        self.Hasil = np.array([round(may_merah, 2), round(may_hijau, 2),
+                               round(may_biru, 2), round(may_kuning, 2),
+                               round(may_putih, 2)]).argmin() + 1
+        print("\nHasil Perhitungan K-Mean : \nMerah : ", may_merah,
+              "     Hijau : ", may_hijau,
+              "     Biru : ", may_biru,
+              "     Kuning : ", may_kuning,
+              "     Putih : ", may_putih, "\n")
+
+        print(self.Hasil)
+        warna = self.Finis()
+        return warna
+
+    def Finis(self):
+        # 1 = Merah     2 = Hijau      3 = Biru     4 = Kuning      5 = Putih
+        if (self.Hasil == 1):
+            warna = "Merah"
+        elif (self.Hasil == 2):
+            warna = "Hijau"
+        elif (self.Hasil == 3):
+            warna = "Biru"
+        elif (self.Hasil == 4):
+            warna = "Kuning"
+        elif (self.Hasil == 5):
+            warna = "Putih"
+        return warna
+
---
+++ main.py
@@ -0,0 +1,72 @@
+import cv2 as cv
+import sys
+import keyboard
+import Kmean
+
+Cropp_Img = False
+ix, iy, ix_end, iy_end = 0, 0, 0, 0
+
+def Mouse_Crop(event, x, y, flags, param):
+    global ix, iy, ix_end, iy_end, Cropp_Img
+
+    if event == cv.EVENT_LBUTTONDOWN:
+        print("cv.EVENT_LBUTTONDOWN")
+        ix, iy, ix_end, iy_end = x, y, x, y
+        Cropp_Img = True
+        print("ix = ", ix, "iy = ", iy, "\nix_end = ", ix_end, "iy_end = ", iy_end)
+
+    elif event == cv.EVENT_MOUSEMOVE:
+        # print("cv.EVENT_MOUSEMOVE")
+        if Cropp_Img == True:
+            ix_end, iy_end = x, y
+
+    elif event == cv.EVENT_LBUTTONUP:
+        print("cv.EVENT_LBUTTONUP")
+        ix_end, iy_end = x, y
+        Cropp_Img = False
+        print("ix = ", ix, "iy = ", iy, "\nix_end = ", ix_end, "iy_end = ", iy_end)
+        refPoint = [(ix, iy), (ix_end, iy_end)]
+
+        if len(refPoint) == 2:
+            img2 = img_copy[refPoint[0][1]:refPoint[1][1], refPoint[0][0]:refPoint[1][0]]
+            clear = cv.fastNlMeansDenoisingColored(img2)
+
+            show_clear = cv
+            show_clear.imshow("Image Clear ", clear)
+            show_clear.moveWindow("Image Clear", 100, 500)
+            # print("Image Clear = \n", clear)
+            c1 = int(cv.mean(clear)[0])
+            c2 = int(cv.mean(clear)[1])
+            c3 = int(cv.mean(clear)[2])
+            # print(cv.mean(clear))
+            print("\nMean 1 = \n", c1)
+            print("\nMean 2 = \n", c2)
+            print("\nMean 3 = \n", c3)
+
+            final = Kmean.kmean(c3, c2, c1)
+            print(f"RGB {c3, c2, c1} Termasuk dalam Kategori Warna : ", final.check())
+
+
+img = cv.imread('img/Putih/1.png')
+img_copy = img.copy()
+
+cv.namedWindow("image")
+cv.setMouseCallback("image", Mouse_Crop)
+
+while True:
+    i = img.copy()
+
+    if not Cropp_Img:
+        cv.imshow("image", img)
+
+    elif Cropp_Img:
+        cv.rectangle(i, (ix, iy), (ix_end, iy_end), (255, 0, 0), 2)
+        cv.imshow("image", i)
+
+    if keyboard.is_pressed('Esc'):
+        print("Exit")
+        sys.exit(0)
+    cv.waitKey(1)
+
+cv2.destroyAllWindows()
+
